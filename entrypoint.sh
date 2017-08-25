4fcc661164c994e03a76c4b54fe832490a2cfe2c
Kangxu Liu <liukangxu1996@gmail.com>
2017-08-25 03:55:37 +0000

    Add webgui plugin

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-SS_METHOD=${SS_METHOD:-"aes-128-cfb"}
+SS_METHOD=${SS_METHOD:-"aes-256-cfb"}
 SS_MANAGER_ADDRESS=${SS_MANAGER_ADDRESS:-"127.0.0.1:6001"}
+SS_MANAGER_WEBGUI=${SS_MANAGER_WEBGUI:false}
 
-while getopts "m:a:" OPT; do
+while getopts "m:a:w" OPT; do
     case $OPT in
         m)
             SS_METHOD=$OPTARG;;
         a)
             SS_MANAGER_ADDRESS=$OPTARG;;
+        w)
+            SS_MANAGER_WEBGUI=true;;
     esac
 done
 
-echo -e "\033[32mStarting shadowsocks...\033[0m"
-ss-manager -u -m $SS_METHOD -u --manager-address $SS_MANAGER_ADDRESS &
+if [ "$SS_MANAGER_WEBGUI" = true ]; then
+    echo -e "\033[32mStarting ssmgr webgui...\033[0m"
+    /usr/bin/ssmgr -c /root/.ssmgr/webgui.yml
+else
+    echo -e "\033[32mStarting shadowsocks...\033[0m"
+    ss-manager -u -m $SS_METHOD -u --manager-address $SS_MANAGER_ADDRESS &
 
-echo -e "\033[32mStarting ssmgr slave...\033[0m"
-/usr/bin/ssmgr -c /root/.ssmgr/default.yml
+    echo -e "\033[32mStarting ssmgr slave...\033[0m"
+    /usr/bin/ssmgr -c /root/.ssmgr/default.yml
+fi
